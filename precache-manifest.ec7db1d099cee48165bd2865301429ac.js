340f1cd1dd9ac7384f8a4f749778faa29067947c
Robin Lallier <robin.lallier29@gmail.com>
2019-10-14 14:26:29 +0300

    Updates

---
+++ precache-manifest.ec7db1d099cee48165bd2865301429ac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea8a79d675298adb8a2fc6a2314120e9",
+    "url": "/dotk/index.html"
+  },
+  {
+    "revision": "50f147288788e8b44267",
+    "url": "/dotk/static/css/main.e2273ee0.chunk.css"
+  },
+  {
+    "revision": "91447c7b62e1cbb264fa",
+    "url": "/dotk/static/js/2.e1a45b6d.chunk.js"
+  },
+  {
+    "revision": "50f147288788e8b44267",
+    "url": "/dotk/static/js/main.8c5e11da.chunk.js"
+  },
+  {
+    "revision": "5c36530b19e8187ef184",
+    "url": "/dotk/static/js/runtime~main.f71c18ce.js"
+  }
+]);
